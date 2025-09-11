864c44e30e4c2a166299ba90035ceebc8f30e6af
Eunicekk <kate4234@gmail.com>
2025-09-12 01:11:01 +0900

    feat: 메인 페이지 작업

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -10,11 +10,37 @@ export default function Home() {
           <Link to="/profile">프로필</Link>
         </div>
       </header>
-      <div className="post-list">Post List</div>
+      <nav className="post__navigation">
+        <div className="post__navigation--active">전체 글</div>
+        <div>나의 글</div>
+      </nav>
+      <div className="post__list">
+        {[...Array(10)].map((e, index) => (
+          <div key={index} className="post__box">
+            <Link to={`/posts/${index}`}>
+              <div className="post__profile-box">
+                <div className="post__profile"></div>
+                <div className="post__author-name">패스트캠퍼스</div>
+                <div className="post__date">2025.09.11 목요일</div>
+              </div>
+              <div className="post__title">게시글 {index}</div>
+              <div className="post__text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, minus magnam quam sed atque quas
+                delectus laboriosam vel molestiae voluptatibus officia quia quaerat reprehenderit magni aspernatur vitae
+                necessitatibus. Expedita, tempora.
+              </div>
+              <div className="post__utils-box">
+                <div className="post__edit">수정</div>
+                <div className="post__delete">삭제</div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
       <footer>
-        <div>Menu 1</div>
-        <div>Menu 2</div>
-        <div>Menu 3</div>
+        <Link to="/posts/new">글쓰기</Link>
+        <Link to="/posts">게시글</Link>
+        <Link to="/profile">프로필</Link>
       </footer>
     </main>
   );

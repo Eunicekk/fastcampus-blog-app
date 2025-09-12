e451f10d85104caebf0cc7dc3721db2aa6a11d51
Eunicekk <kate4234@gmail.com>
2025-09-13 00:58:43 +0900

    feat: 캐러셀 컴포넌트 작업

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,23 +1,23 @@
+import Home from 'pages/home';
 import { Route, Routes, Navigate } from 'react-router-dom';
-import Home from '../pages/home';
-import PostList from '../pages/posts';
-import PostDetail from '../pages/posts/detail';
-import PostNew from '../pages/posts/new';
-import PostEdit from '../pages/posts/edit';
-import Profile from '../pages/profile';
-import Login from '../pages/login';
-import Signup from '../pages/signup';
+import PostNew from 'pages/posts/new';
+import PostEdit from 'pages/posts/edit';
+import PostsPage from 'pages/posts';
+import PostDetailPage from 'pages/posts/detail';
+import ProfilePage from 'pages/profile';
+import Login from 'pages/login';
+import Signup from 'pages/signup';
 
 export default function Router() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/posts" element={<PostList />} />
-        <Route path="/posts/:id" element={<PostDetail />} />
+        <Route path="/posts" element={<PostsPage />} />
+        <Route path="/posts/:id" element={<PostDetailPage />} />
         <Route path="/posts/new" element={<PostNew />} />
         <Route path="/posts/edit/:id" element={<PostEdit />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile" element={<ProfilePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="*" element={<Navigate replace to="/" />} />
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,13 @@
-import Header from '../../components/Header';
-import PostList from '../../components/PostList';
-import Footer from '../../components/Footer';
+import Carousel from 'components/Carousel';
+import Footer from 'components/Footer';
+import Header from 'components/Header';
+import PostList from 'components/PostList';
 
 export default function Home() {
   return (
     <main>
       <Header />
-
+      <Carousel />
       <PostList />
       <Footer />
     </main>

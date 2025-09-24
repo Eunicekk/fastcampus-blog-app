755aed6912b2e82e8f062a461981e4e4c6fd5b08
Eunicekk <kate4234@gmail.com>
2025-09-24 19:17:02 +0900

    feat: Context API를 활용한 사용자 정보 관리

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,27 +1,29 @@
+import AuthContext from 'context/AuthContext';
 import { getAuth, signOut } from 'firebase/auth';
 import { app } from 'firebaseApp';
+import { useContext } from 'react';
 import { toast } from 'react-toastify';
 
+const onSignout = async () => {
+  try {
+    const auth = getAuth(app);
+    await signOut(auth);
+    toast.success('로그아웃이 완료되었습니다.');
+  } catch (error: any) {
+    console.log(error);
+    toast.error(`에러: ${error?.code}`);
+  }
+};
 export default function Profile() {
-  const auth = getAuth(app);
-
-  const onSignout = async () => {
-    try {
-      await signOut(auth);
-      toast.success('로그아웃이 완료되었습니다.');
-    } catch (error: any) {
-      console.log(error);
-      toast.error(`에러: ${error?.code}`);
-    }
-  };
+  const { user } = useContext(AuthContext);
 
   return (
     <div className="profile__box">
       <div className="flex__box-lg">
         <div className="profile__image" />
         <div>
-          <div className="profile__email">{auth?.currentUser?.email}</div>
-          <div className="profile__name">{auth?.currentUser?.displayName || '사용자'}</div>
+          <div className="profile__email">{user?.email}</div>
+          <div className="profile__name">{user?.displayName || '사용자'}</div>
         </div>
       </div>
       <div role="presentation" className="profile__logout" onClick={onSignout}>

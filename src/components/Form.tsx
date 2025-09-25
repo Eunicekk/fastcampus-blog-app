e83b91e3992e4f7f7a80e634a5e0a12f15f7d46d
Eunicekk <kate4234@gmail.com>
2025-09-25 18:32:50 +0900

    feat: firestore로 데이터 생성: 게시물 작성

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,67 @@
+import AuthContext from 'context/AuthContext';
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from 'firebaseApp';
+import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
 export default function PostForm() {
+  const { user } = useContext(AuthContext);
+  const [title, setTitle] = useState<string>('');
+  const [summary, setSummary] = useState<string>('');
+  const [content, setContent] = useState<string>('');
+  const navigate = useNavigate();
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      await addDoc(collection(db, 'posts'), {
+        title: title,
+        summary: summary,
+        content: content,
+        createAt: new Date()?.toLocaleDateString(),
+        email: user?.email,
+      });
+
+      toast?.success('게시물 작성이 완료되었습니다.');
+      navigate('/');
+    } catch (error: any) {
+      toast?.error(`에러: ${error?.code}`);
+    }
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === 'title') {
+      setTitle(value);
+    }
+
+    if (name === 'summary') {
+      setSummary(value);
+    }
+
+    if (name === 'content') {
+      setContent(value);
+    }
+  };
+
   return (
-    <form action="/post" method="post" className="form">
+    <form onSubmit={onSubmit} className="form">
       <div className="form__block">
         <label htmlFor="title">제목</label>
-        <input type="text" name="title" id="title" required />
+        <input type="text" name="title" id="title" required onChange={onChange} value={title} />
       </div>
       <div className="form__block">
         <label htmlFor="summary">요약</label>
-        <input type="text" name="summary" id="summary" required />
+        <input type="text" name="summary" id="summary" required onChange={onChange} value={summary} />
       </div>
       <div className="form__block">
         <label htmlFor="conteent">내용</label>
-        <textarea name="content" id="content" required />
+        <textarea name="content" id="content" required onChange={onChange} value={content} />
       </div>
       <div className="form__block">
         <input type="submit" value="제출" className="form__btn--submit" />
